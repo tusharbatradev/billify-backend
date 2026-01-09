7cfedd4c9f525b3315aa28f4d5eda0106e534d5f
Tushar Batra <tusharbatra08@gmail.com>
2026-01-09 17:01:11 +0530

    Invoice CRUD done succesfully

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const auth = require("./Middlewares/auth");
 const productRouter = require("./Routes/products");
 const customerRouter = require("./Routes/customers");
+const InvoiceRouter = require("./Routes/invoice");
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use(cors(corsOption));
 app.use("/user", businessRouter);
 app.use("/product", auth, productRouter);
 app.use("/customer", auth, customerRouter);
+app.use("/invoice", auth, InvoiceRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
